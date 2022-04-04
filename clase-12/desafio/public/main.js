56f9122f963a89c55b89279867d534e50eb255a0
ezeroti <ezecrt@gmail.com>
2022-04-04 03:45:13 -0300

    change

--- clase-12/desafio/public/main.js
+++ clase-12/desafio/public/main.js
@@ -1,23 +1,21 @@
 const socket = io();
 
-function render(data) {
-        var tbodyRef = document.getElementById('addProduct')
+socket.on('productList', function(data) { 
+    var tbodyRef = document.getElementById('addProduct')
         
-        var newRow = tbodyRef.insertRow();
-        var newCellTitle = newRow.insertCell();
-        var newCellPrice = newRow.insertCell();
-        var newCellThumbnail = newRow.insertCell();
+    var newRow = tbodyRef.insertRow();
+    var newCellTitle = newRow.insertCell();
+    var newCellPrice = newRow.insertCell();
+    var newCellThumbnail = newRow.insertCell();
 
-        var newTextTitle = document.createTextNode(data.title);
-        var newTextPrice = document.createTextNode(data.price);
-        var newTextThumbnail = document.createTextNode(data.thumbnail);
-        
-        newCellTitle.appendChild(newTextTitle);
-        newCellPrice.appendChild(newTextPrice);
-        newCellThumbnail.appendChild(newTextThumbnail);
-}
-
-socket.on('productList', function(data) { render(data); });
+    var newTextTitle = document.createTextNode(data.title);
+    var newTextPrice = document.createTextNode(data.price);
+    var newTextThumbnail = document.createTextNode(data.thumbnail);
+    
+    newCellTitle.appendChild(newTextTitle);
+    newCellPrice.appendChild(newTextPrice);
+    newCellThumbnail.appendChild(newTextThumbnail);
+ });
 
 function addProduct(e) {
     const product = {
